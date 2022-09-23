2daabda77015d8ca3892f8637a324de160033f3c
eddie <eddie@eddies-MacBook-Pro.local>
2022-09-23 18:00:41 +0700

    added adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
 
 	kit: {
-		adapter: adapter(),
+		// default options are shown
+        adapter: vercel({
+            edge: false,
+            external: [],
+            split: false
+        }),
 
 		prerender: {
 			default: true,
